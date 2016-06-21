b81f16e5f5c27221ba03168d8418560c35474fb4
Panepo <whisper.ko@gmail.com>
2016-06-21 10:01:33 +0800

    modify settings and layout
    modify eslint settings
    modify drawer layout

--- src/containers/Drawer.js
+++ src/containers/Drawer.js
@@ -16,8 +16,14 @@ class Drawer extends Component {
 		
 		return (
 			<div className="mdl-layout__drawer">
-				<span className="mdl-layout-title">Config</span>
+				<span className="mdl-layout-title">Licence</span>
 				<nav className="mdl-navigation">
+					<a className="mdl-navigation__link" href="http://www.dmm.com/netgame_s/kancolle/">艦これ</a>
+					<a className="mdl-navigation__link" href="https://facebook.github.io/react/">React</a>
+					<a className="mdl-navigation__link" href="http://redux.js.org/">Redux</a>
+					<a className="mdl-navigation__link" href="https://d3js.org/">D3</a>
+					<a className="mdl-navigation__link" href="https://getmdl.io/">Material Design Lite</a>
+					<a className="mdl-navigation__link" href="http://lokijs.org/">LokiJS</a>
 				</nav>
 				<Footer />
 			</div>
