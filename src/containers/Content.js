6f01a9e383f58b1326f82a6ab272ae6b0739827e
Panepo <whisper.ko@gmail.com>
2016-07-07 18:03:41 +0800

    add branch
    new branch for KamoLite

--- src/containers/Content.js
+++ src/containers/Content.js
@@ -1,62 +1,20 @@
 import React, { Component, PropTypes } from 'react'
-import { connect } from 'react-redux'
 import FleetGroup from './FleetGroup'
-import FleetStatus from './FleetStatus'
-import FleetMap from './FleetMap'
-import { listStatus, listStatusS } from '../constants/ConstList'
+import { listStatusS } from '../constants/ConstList'
 import '../../css/Content.css'
 
-class Content extends Component {
+export default class Content extends Component {
 	componentDidUpdate() {
 		componentHandler.upgradeDom()
 	}
 	
 	render() {
-		const { dispStatus } = this.props
-		
-		var dispTemp
-		switch ( dispStatus ) {
-			case listStatusS[0]:
-				dispTemp = 
-					<div className={listStatusS[0] + " display-area"}>
-						<FleetGroup />
-					</div>
-				break
-			case listStatusS[1]:
-				dispTemp = 
-					<div className={listStatusS[1] + " display-area"}>
-						<FleetStatus />
-					</div>
-				break
-			case listStatusS[2]:
-				dispTemp = 
-					<div className={listStatusS[2] + " display-area"}>
-						<FleetMap />
-					</div>
-				break
-			default:
-				dispTemp = <div className={dispStatus}>{dispStatus}</div>
-		}
-		
-		
 		return (
 			<main className="content mdl-layout__content">
-				{dispTemp}
+				<div className={listStatusS[0] + " display-area"}>
+					<FleetGroup />
+				</div>
 			</main>
 		)
 	}
 }
-
-Content.propTypes = {
-	dispStatus: PropTypes.string.isRequired
-}
-
-const mapStateToProps = (state) => {
-	return {
-		dispStatus: state.reducerPage.status
-	}
-}
-
-export default connect(
-	mapStateToProps
-)(Content)
--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,31 +1,14 @@
 import React, { Component, PropTypes } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import { pageChange, modelOpen, modelClose } from '../actions'
-import ToggleButton from '../components/ToggleButton'
+import { modelOpen, modelClose } from '../actions'
 import TextModel from '../components/TextModel'
 import { listStatus, listStatusS } from '../constants/ConstList'
 import '../../css/Header.css'
 
 class Header extends Component {
 	render() {
-		const { pageStatus, pageChange, modelStatus, modelOpen, modelClose, textOutput } = this.props
-		
-		var buttonTemp
-		var buttonOut = []
-		for (var i=0; i<listStatus.length; i++){
-			buttonTemp = (
-				<ToggleButton
-					key={"statusButton" + i.toString()}
-					modelId={listStatusS[i]}
-					display={pageStatus}
-					onClickFunc={(modelId) => pageChange(modelId)}
-					Cactive={"mdl-layout__tab is-active"}
-					Cinactive={"mdl-layout__tab"}
-					title={listStatus[i]} />
-			)
-			buttonOut.push(buttonTemp)
-		}
+		const { modelStatus, modelOpen, modelClose, textOutput } = this.props
 		
 		return (
 			<header className="mdl-layout__header">
@@ -34,7 +17,7 @@ class Header extends Component {
 					input={textOutput} 
 					modelClose={() => modelClose(0)} />
 				<div className="mdl-layout__header-row">
-					<span className="mdl-layout-title">KAMO</span>
+					<span className="mdl-layout-title">KAMO Lite</span>
 					<div className="mdl-layout-spacer"></div>
 					<nav className="mdl-navigation mdl-layout--large-screen-only">
 						<a className="mdl-navigation__link" href="http://www.dmm.com/netgame_s/kancolle/">艦これ</a>
@@ -42,23 +25,18 @@ class Header extends Component {
 						<a className="mdl-navigation__link" onClick={() => modelOpen(0)}>掲示板張り付け用</a>
 					</nav>
 				</div>
-				<div className="mdl-layout__tab-bar mdl-js-ripple-effect">
-					{buttonOut}
-				</div>
 			</header>
 		)
 	}
 }
 
 Header.propTypes = {
-	pageStatus: PropTypes.string.isRequired,
 	modelStatus: PropTypes.bool.isRequired,
 	textOutput: PropTypes.array.isRequired
 }
 
 const mapStateToProps = (state) => {
 	return {
-		pageStatus: state.reducerPage.status,
 		modelStatus: state.reducerPage.modelStatus,
 		textOutput: state.reducerPage.textOutput
 	}
@@ -66,7 +44,6 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
 	return {
-		pageChange: bindActionCreators(pageChange, dispatch),
 		modelOpen: bindActionCreators(modelOpen, dispatch),
 		modelClose: bindActionCreators(modelClose, dispatch)
 	}
